ed9cc0f9190963471f8858ee4a59873968c6c3a3
supermancbs <clark.sacktor@gmail.com>
2016-06-23 14:35:12 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,9 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    binding.pry
+    self.all.pluck(:name).first(5)
+  end 
 end
